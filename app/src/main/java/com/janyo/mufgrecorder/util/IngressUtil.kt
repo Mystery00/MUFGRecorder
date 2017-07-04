0c2f4249042d6dea5d212c18e022bcd4d2e38d3e
Mystery00 <mystery0dyl520@gmail.com>
2017-07-04 12:55:07 +0800

    数据存储查错完成

--- app/src/main/java/com/janyo/mufgrecorder/util/IngressUtil.kt
+++ app/src/main/java/com/janyo/mufgrecorder/util/IngressUtil.kt
@@ -6,34 +6,32 @@ import java.util.HashMap
 
 class IngressUtil(val context: Context)
 {
-
 	fun ConvertArrayToList(checked: BooleanArray,
 						   items: Array<String>): ArrayList<HashMap<String, Any>>
 	{
 		val item_level = context.resources.getStringArray(R.array.ingress_items_level_things)
 		val level = context.resources.getStringArray(R.array.ingress_level_things)
 		val list = ArrayList<HashMap<String, Any>>()
-		val map = HashMap<String, Any>()
 		for (i in checked.indices)
 		{
 			if (!checked[i])
 			{
 				continue
 			}
+			val map = HashMap<String, Any>()
 			map.put("name", items[i])
 			map.put("number", 0)
 			if (item_level.contains(items[i]))
 			{
 				map.put("type", 1)
-				list.add(map)
-				continue
+				map.put("level", 0)
 			}
 			if (level.contains(items[i]))
 			{
 				map.put("type", 2)
-				list.add(map)
-				continue
+				map.put("level", 0)
 			}
+			list.add(map)
 		}
 		return list
 	}
